de0600e59a86b584c19b34c0741e6161a1aef12e
Pablo Nussembaum <nussembaum@corporater.com>
2013-04-23 19:22:19 -0300

    generates nodes transitions in dot format

--- src/main/clojure/mentat/graph.clj
+++ src/main/clojure/mentat/graph.clj
@@ -1,24 +1,42 @@
 (ns mentat.graph
   (:require [clojure.reflect :as r] 
             [mentat.core :as t]
-            [clojure.string :only (join)])
+            [clojure.string :only (join) :as s])
   (:import (java.lang.reflect Method Modifier Field)))
-  
-  (defn short-method-name
-    "shorts the name of a method"
-    [pre]
-    (let [m (first pre)
-          v (second pre)]
-      (str (if v "" "&not;") 
-         (.getSimpleName (.getDeclaringClass m)) "." 
-         (.getName m) "("
-         (join "," (map #(.getSimpleName %) (.getParameterTypes m)))
-         ")")))
-    
 
-  (defn state-name
-    "generates a state name"
-    [pres]
-    (join "\\n" (map short-method-name pres)))
-  
+(defn method-label
+  ""
+  [^Method m]
+  (str (.getSimpleName (.getDeclaringClass m)) "." 
+       (.getName m) 
+       "(" (s/join "," (map #(.getSimpleName %) (seq (.getParameterTypes m)))) ")"))
 
+(defn pre-name
+  "shorts the name of a method"
+  [pre-val]
+  (let [m (first pre-val)
+        v (second pre-val)]
+    (str (if v "" "&not;") (method-label m))))
+
+(defn state-name
+  "generates a state name"
+  [pres]
+  (str "\"" (s/join "\\n" (map pre-name pres)) "\""))
+
+(defn build-finite-state-machine
+  "generates a finite state machine based on coll of pres"
+  ([coll] 
+    (if (empty? coll) #{}
+      (let [pres (first coll)
+            rcoll (rest coll) 
+            [_ state] pres 
+            st-name (state-name state)]
+        (build-finite-state-machine #{(str "start -> " st-name "")} rcoll st-name))))
+  ([so-far coll cur-state]
+    (if (empty? coll) so-far
+      (let [pres (first coll) 
+            [method state] pres 
+            rcoll (rest coll) 
+            st-name (state-name state)
+            m-label (method-label method)]
+        (recur (conj so-far (str cur-state " -> " st-name "[ label = \"" m-label "\" ]")) rcoll st-name)))))
