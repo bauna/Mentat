b95a65d9c613ba7323a75c75ded2d6e8122072f7
Pablo Nussembaum <nussembaum@corporater.com>
2013-06-04 18:43:54 -0300

    baby steps

--- src/main/clojure/mentat/selDSL.clj
+++ src/main/clojure/mentat/selDSL.clj
@@ -1,12 +1,18 @@
 (ns mentat.selDSL
   (:require [clojure.edn :only (read) :as e]))
 
-(defn read-config-file [filename]
-  (with-open [r (java.io.PushbackReader.
-                 (clojure.java.io/reader filename))]
-    (e/read r)))
-
 (defn gen-fn
   [body]
   (let [as [(symbol "vs")]]
     (eval `(fn ~as ~body))))
+
+(defn while-tag-parser [a]
+  (println a)
+  [(first a) (gen-fn (second a)) ])
+
+(def tag-parsers {(symbol "mentat/while") while-tag-parser})
+
+(defn read-config-file [filename]
+  (with-open [r (java.io.PushbackReader.
+                 (clojure.java.io/reader filename))]
+    (e/read {:readers tag-parsers} r)))
