05ce9241aad45253d77ea17fbcc5709127c0af2e
Pablo Nussembaum <baunax@gmail.com>
2013-06-21 13:58:25 -0300

    add depedency to z3

--- project.clj
+++ project.clj
@@ -6,5 +6,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.flatland/ordered "1.5.1"]]
-  :dev-dependencied [[no-man-is-an-island/lein-eclipse "2.0.0"]])
+                 [org.flatland/ordered "1.5.1"]
+		 [com.microsoft/z3 "4.3.2.Uecceb0ac"]]
+  :dev-dependencied [[no-man-is-an-island/lein-eclipse "2.0.0"]]
+  :jvm-opts [~(str "-Djava.library.path=native/" 
+                   (. System getProperty "path.separator") 
+                   (. System getProperty "java.library.path"))])
