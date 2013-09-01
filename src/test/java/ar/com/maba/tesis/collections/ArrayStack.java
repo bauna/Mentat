0436a935a59bc74e296adeea2fcf3e0210d4f64c
Maria Bonfiglio <bonfigliomaria@gmail.com>
2013-09-01 16:44:20 -0300

    Moved to test

--- src/test/java/ar/com/maba/tesis/collections/ArrayStack.java
+++ src/test/java/ar/com/maba/tesis/collections/ArrayStack.java
@@ -1,19 +1,33 @@
 package ar.com.maba.tesis.collections;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import clojure.main;
 
 import ar.com.maba.tesis.preconditions.ClassDefinition;
 import ar.com.maba.tesis.preconditions.Pre;
 
 @ClassDefinition(builder="(new ar.com.maba.tesis.collections.ArrayStack 5)",
         invariant = "(>= (:limit vs) (count (:stack vs)))")
-public class ArrayStack<T> implements Stack<T> {
-    private List<T> stack = new ArrayList<>();
+public class ArrayStack implements Stack<Integer> {
+    private List<Integer> stack = new ArrayList<>();
     private final int limit;
     
     public ArrayStack(int limit) {
         this.limit = limit;
+		try {
+			Field f;
+			f = getClass().getDeclaredField("stack");
+			f.getDeclaringClass();
+			Class<?> c = (Class<?>) ((ParameterizedType)f.getGenericType()).getActualTypeArguments()[0];
+			System.out.println("ñlfk: " + c.getName());
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		}
     }
 
     /**
@@ -21,11 +35,13 @@ public class ArrayStack<T> implements Stack<T> {
      */
     @Override
     @Pre(value = "(> (:limit vs) (count (:stack vs)))", data = "\"foo\"")
-    public void push(T o) {
+    public void push(Integer o) {
         if (limit == stack.size()) {
             throw new IllegalStateException("Limit reached");
         }
         stack.add(o);
+        if (stack.size() > 3)
+        	throw new RuntimeException();
     }
     
     /**
@@ -33,7 +49,7 @@ public class ArrayStack<T> implements Stack<T> {
      */
     @Override
     @Pre("(not (empty? (:stack vs)))")
-    public T pop() {
+    public Integer pop() {
         if (stack.isEmpty()) {
             throw new IllegalStateException("Stack is empty");
         }
@@ -44,5 +60,9 @@ public class ArrayStack<T> implements Stack<T> {
     @Pre(enabled = false)
     public String toString() {
         return stack.toString();
+    }
+    
+    public static void main(String args[]) {
+    	System.out.println(Boolean.TYPE.getName());
     }
 }
