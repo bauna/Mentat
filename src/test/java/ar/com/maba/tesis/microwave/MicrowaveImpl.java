e30809f67485cec0e57a8cb19fc09a5ce464c324
Pablo Nussembaum <nussembaum@corporater.com>
2013-09-26 16:38:48 -0300

    cannot start if the door is open

--- src/test/java/ar/com/maba/tesis/microwave/MicrowaveImpl.java
+++ src/test/java/ar/com/maba/tesis/microwave/MicrowaveImpl.java
@@ -25,19 +25,19 @@ public class MicrowaveImpl implements Microwave {
 	private Integer time = 0;
 
 	@Override
-	@Pre(value = "(not on)")
+	@Pre(value = "(and (not doorOpened) (not on))")
 	public void start() {
 		start(DEFAULT_TIME, DEFAULT_POWER);
 	}
 
 	@Override
-	@Pre(value = "(and (not on) (> p0 0))")
+	@Pre(value = "(and (not doorOpened) (not on) (> p0 0))")
 	public void start(Integer time) {
 		start(time, DEFAULT_POWER);
 	}
 
 	@Override
-	@Pre(value = "(and (not on) (> p0 0) (> p1 0))")
+	@Pre(value = "(and (not doorOpened) (not on) (> p0 0) (> p1 0))")
 	public void start(Integer time, Integer power) {
 		if (doorOpened || on) {
 			System.out.println("start(Integer time, Integer power)");
