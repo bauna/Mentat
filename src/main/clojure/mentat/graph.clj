8801e6077a9f699b49d1ceae84d6fc1b2471b187
Pablo Nussembaum <baunax@gmail.com>
2014-04-28 08:25:03 -0300

    build the dot file using several traces

--- src/main/clojure/mentat/graph.clj
+++ src/main/clojure/mentat/graph.clj
@@ -1,5 +1,6 @@
 (ns mentat.graph
-  (:require [clojure.reflect :as r] 
+  (:require [clojure.reflect :as r]
+            [clojure.set :as set]
             [mentat.core :as t]
             [flatland.ordered.map :only (ordered-map) :as om]
             [clojure.string :only (join) :as s]
@@ -9,52 +10,43 @@
 (defn ^String method-label
   "build a string representation for a method"
   [^Method m]
-  (str (.getName m) 
-       "(" (s/join ", " (map #(.getSimpleName %) (seq (.getParameterTypes m)))) ")"))
+  (if m (str (.getName m) 
+            "(" (s/join ", " (map #(.getSimpleName %) (seq (.getParameterTypes m)))) ")")))
+
+(defn- failed? 
+  [pre] 
+  (= :failed (second pre)))
 
 (defn ^String state-name
   "generates a state name"
-  [pres]
-  (if (= :failed pres) :trap
-    (s/join "\\n" (map #(method-label (first %)) (filter second pres)))))
+  [pre]
+  (if (failed? pre) :trap
+    (s/join "\\n" (map #(method-label (first %)) (filter second pre)))))
 
 (defn build-finite-state-machine
-  "generates a finite state machine based on coll of pres"
-  [coll]
-  (let [mem-state-name (memoize state-name)
-        func (fn f 
-               ([coll] 
-                 (if-let [s (seq coll)] 
-                   (let [pres (first s)
-                         rcoll (rest s) 
-                         [_ state] pres 
-                         st-name (mem-state-name state)]
-                     (if (= :failed state) 
-                       [(om/ordered-map [:start :trap]  #{}) true]
-                       (f (om/ordered-map [:start  st-name] #{}) rcoll st-name)))))
-               ([so-far coll cur-state]
-                 (if-let [s (seq coll)]
-                   (let [pres (first coll) 
-                         [method state] pres
-                         m-label (method-label method)]
-                     (if (= :failed state)
-                       [(assoc so-far [cur-state :trap] #{m-label}) true]  
-                       (let [rcoll (rest coll) 
-                             st-name (mem-state-name state)
-                             transition [cur-state st-name]]
-                         (recur (assoc so-far transition
-                                       (if-let [labels (so-far transition)] 
-                                         (conj labels m-label) #{m-label})) 
-                                      rcoll st-name))))
-                   [so-far false])))]
-    (func coll)))
+  ([max-steps traces]
+    (let [mem-state-name (memoize state-name)
+          mem-method-label (memoize method-label)
+          gen-step (fn [[method state :as pres]] [(mem-state-name state) (mem-method-label method)])
+          union #(let [s1 (if %1 %1 #{})] (if %2 (set/union s1 #{%2}) s1))
+          gen-state-machine (fn [trace] 
+                              (first (reduce 
+                                       (fn [[m curr-state :as _] pre] 
+                                         (let [[state-name label] (gen-step pre)
+                                               transition [curr-state state-name]]
+                                           [(update-in m [transition] union label) state-name])) 
+                                       [(om/ordered-map) :start] trace)))] 
+      (reduce (fn [full-machine m] 
+                (reduce (fn [full-machine [k v :as _]] 
+                          (update-in full-machine [k] set/union v)) full-machine m)) 
+              (map #(gen-state-machine (take max-steps %)) traces)))))
 
-(defn- mk-label
-  [labels]
-  (s/join "\\n" labels))
+(def ^:dynamic *max-steps* 100) 
 
 (defn build-dot-file
-  [coll]
-  (let [[transitions has-trap] (build-finite-state-machine coll)]
-    (d/digraph (into [{:rankdir "LR"} [:start {:shape "doublecircle"}]] 
-                     (map #(conj (first %) (if (empty? (second %)) {} {:label (mk-label (second %))})) transitions)))))
+  ([traces] (build-dot-file *max-steps* traces))
+  ([max-steps traces] (let [transitions (build-finite-state-machine max-steps traces)]
+     (d/digraph (into [{:rankdir "LR"} [:start {:shape "doublecircle"}]] 
+                      (map (fn [[transition labels :as _]] 
+                             (conj transition (if (empty? labels) {} {:label (s/join "\\n" labels)}))) 
+                           transitions))))))
