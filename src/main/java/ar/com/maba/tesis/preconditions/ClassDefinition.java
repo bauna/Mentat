9e60df897f51c32e592ab000ccf772963084a92d
Pablo Nussembaum <baunax@gmail.com>
2014-05-31 19:04:03 -0300

    javadoc

--- src/main/java/ar/com/maba/tesis/preconditions/ClassDefinition.java
+++ src/main/java/ar/com/maba/tesis/preconditions/ClassDefinition.java
@@ -6,10 +6,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Class annotation for defining the invariant and instance builder. 
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ClassDefinition {
+
+    /** Defines the invariant expression */
     String invariant() default "true";
+
+    /** Defines an instance creation function */
     String builder();
 }
--- src/main/java/ar/com/maba/tesis/preconditions/Pre.java
+++ src/main/java/ar/com/maba/tesis/preconditions/Pre.java
@@ -6,12 +6,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *  Annotation for defining a method precondition.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Pre {
+    /** Defines the precondition expression */
     String value() default "true";
+
+    /** Defines a parameter definition function */
     String data() default "";
+
+    /** Defines if this method should be included in the analysis */
     boolean enabled() default true;
+
+    /** Defines an alternative name for the method */
     String name() default "";
 }
