a4f2c96709f792aaff23aee716851743d92a0874
Pablo Nussembaum <nussembaum@corporater.com>
2014-06-13 11:22:36 -0300

    bump version

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :test-paths ["test" "src/test/clojure"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.flatland/ordered "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.flatland/ordered "1.5.2"]
                  [com.microsoft/z3 "4.3.2.Uecceb0ac"]
                  [dorothy "0.0.4"]]
   :dev-dependencied [[no-man-is-an-island/lein-eclipse "2.0.0"]]
