ee03230d63941a9dbaace48865a710b9d3eb1c63
Pablo Nussembaum <baunax@gmail.com>
2014-07-30 17:20:07 -0300

    update z3

--- project.clj
+++ project.clj
@@ -6,9 +6,10 @@
   :test-paths ["test" "src/test/clojure"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"local" "file:repo"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.flatland/ordered "1.5.2"]
-                 [com.microsoft/z3 "4.3.2.Uecceb0ac"]
+                 [com.microsoft/z3 "4.3.2.06a4a3599"]
                  [dorothy "0.0.4"]]
   :dev-dependencied [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :jvm-opts [~(str "-Djava.library.path=native/" 
